9997a76dfed3e0327e7664b5abd31db94ed202eb
MichaelKhn <michael.horishman123@gmail.com>
2026-06-17 23:53:44 +0300

    new faeture

--- test_git/main.cpp
+++ test_git/main.cpp
@@ -7,6 +7,9 @@ int main()
 	string str = " and world";
 	cout << "Hello gIT" + str << endl;
 	cout << "this is new string" << endl;
+	int a = 5;
+	int b = 8;
+	cout << a + b << endl;
 	
 	return 0;
 }
